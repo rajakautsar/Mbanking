ec115333fe9d5725995f0a71caf99f65906b73db
rajakautsar <rajakautsar09@gmail.com>
2025-09-21 22:57:57 +0700

    Resolve merge conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Nusantara Bank',
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
       theme: ThemeData(primarySwatch: Colors.blue),
-=======
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // Routing aplikasi
->>>>>>> 7932c1c7d79a013a10b7e981d6326f11d0e4e375
       initialRoute: '/',
       routes: {
         '/': (context) => const LoginScreen(),
